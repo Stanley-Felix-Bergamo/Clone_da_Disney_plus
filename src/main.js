e78100cf083df9e5e68a48adaf5d85fe7a354fd5
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-02-07 15:16:17 -0300

    add section faq

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     mostrandoAbas();
-
+    abreOuFechaResposta();
 });
 
+function abreOuFechaResposta() {
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    for (let i = 0; i < questions.length; i++) {
+
+        questions[i].addEventListener('click', function (elemento) {
+
+            const classe = 'faq__questions__item--is-open';
+            const elementoPai = elemento.target.parentNode;
+
+            elementoPai.classList.toggle(classe);
+        })
+    }
+}
 
 function mostrandoAbas() {
     const buttons = document.querySelectorAll('[data-tab-button]');
