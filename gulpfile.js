c5567017aac2c9efecfed9c340da12fa9203d69f
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2023-02-08 13:33:29 -0300

    comfigurando o udlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,8 +16,15 @@ function images() {
 }
 
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
